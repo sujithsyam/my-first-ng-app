7f8c44af79152a8299a23ac7d5737e23bdfbe588
sujithsyam <syamsujith@gmail.com>
2018-06-03 15:36:44 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,12 +16,12 @@ describe('AppComponent', () => {
   it(`should have as title 'My First App'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('My First Application');
+    expect(app.title).toEqual('My First Angular5 Application');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to My First Application!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to My First Angular5 Application!');
   }));
 });
